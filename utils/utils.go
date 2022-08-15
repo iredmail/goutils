65b35bf7b0414720b8dc2da93c8ffa23d7673a9e
ly020044 <ly020044@gmail.com>
2022-08-15 17:36:11 +0800

    add intersect in utils

---
+++ utils/utils.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"golang.org/x/exp/slices"
+)
+
+func intersect[T comparable](s1, s2 []T) []T {
+	set := make([]T, 0)
+	for _, v := range s1 {
+		if slices.Contains(s2, v) {
+			set = append(set, v)
+		}
+	}
+
+	return set
+}
