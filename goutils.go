4f204fb76d4352686e837c02db5524b255c60445
Zhang Huangbin <zhb@iredmail.org>
2022-08-20 00:06:21 +0800

    New func: IsUUID.

--- goutils.go
+++ goutils.go
@@ -4,7 +4,18 @@ import (
 	"reflect"
 
 	"golang.org/x/exp/slices"
+	"github.com/google/uuid"
 )
+
+func IsUUID(u string) bool {
+	if len(u) == 0 {
+		return false
+	}
+
+	_, err := uuid.Parse(u)
+
+	return err == nil
+}
 
 // IsEmpty
 // supported: string, []any, ptr
