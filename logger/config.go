19f478ef2f0bb25e8376ed26856372d76c5f4630
Zhang Huangbin <zhb@iredmail.org>
2023-03-03 08:33:47 +0800

    内部字段改名，增加注释。

--- logger/config.go
+++ logger/config.go
@@ -2,80 +2,111 @@ package logger
 
 func ConfigWithFile(logFilePath string) *Config {
 	return &Config{
-		logTarget:     "file",
-		logFile:       logFilePath,
-		logBufferSize: 0,
+		target:     "file",
+		logFile:    logFilePath,
+		bufferSize: 0,
 	}
 }
 
 func ConfigWithSys(logSyslogServer, logSyslogTag string) *Config {
 	return &Config{
-		logTarget:       "syslog",
-		logSyslogServer: logSyslogServer,
-		logSyslogTag:    logSyslogTag,
+		target:       "syslog",
+		syslogServer: logSyslogServer,
+		syslogTag:    logSyslogTag,
 	}
 }
 
 type Config struct {
-	logTarget         string
-	logLevel          string
-	logSyslogServer   string
-	logSyslogTag      string
-	logFile           string
-	logMaxSize        int
-	logRotateInterval string
+	target         string // log target: file, syslog
+	level          string // log level: info, warn, error, debug
+	syslogServer   string
+	syslogTag      string
+	logFile        string
+	maxSize        int
+	rotateInterval string // rotate interval. e.g. `12h` (12 hours), `1d` (1 day), `1w` (1 week), `1m` (1 month)
+	maxBackups     int
+	timeFormat     string
+	compress       bool // compress rotated log file
+
 	// Buffer size defaults to (8 * 1024).
 	// Write to log file immediately if size is 0.
-	logBufferSize int
-	logMaxBackups int
-	logTimeFormat string
-	logCompress   bool // compress rotated log file
+	bufferSize int
 }
 
+// SetTarget 控制将 log 记录到哪里。
+// 允许的值：file, syslog。其余值按 `file` 处理。
 func (c *Config) SetTarget(target string) *Config {
-	c.logTarget = target
+	switch target {
+	case "file", "syslog":
+		c.target = target
+	default:
+		c.target = "file"
+	}
 
 	return c
 }
 
+// SetLevel 设置 log level。
+// 允许的值：info, warn, error, debug。其余值按 `info` 处理。
 func (c *Config) SetLevel(level string) *Config {
-	c.logLevel = level
+	switch level {
+	case "info", "warn", "error", "debug":
+		c.level = level
+	default:
+		c.level = "info"
+	}
 
 	return c
 }
 
+// SetMaxSize 设置 log 文件的最大大小（单位 bytes），达到该值则触发 rotate。
 func (c *Config) SetMaxSize(maxSize int) *Config {
-	c.logMaxSize = maxSize
+	if maxSize > 0 {
+		c.maxSize = maxSize
+	} else {
+		c.maxSize = 500 * 1024 * 1024 // 500 MB
+	}
 
 	return c
 }
 
-func (c *Config) SetRotateInterval(rotateInterval string) *Config {
-	c.logRotateInterval = rotateInterval
+// SetRotateInterval 设置 log 文件的 rotate 时间间隔。
+// 示例：`12h` (12 hours), `1d` (1 day), `1w` (1 week), `1m` (1 month)。
+func (c *Config) SetRotateInterval(interval string) *Config {
+	c.rotateInterval = interval
 
 	return c
 }
 
+// SetBufferSize 设置 log 内容的缓冲大小，达到该值后再写入 log target。
+// 设置为 0 表示不做缓冲，小于 0 的值按 0 处理。
 func (c *Config) SetBufferSize(bufferSize int) *Config {
-	c.logBufferSize = bufferSize
+	if bufferSize >= 0 {
+		c.bufferSize = bufferSize
+	} else {
+		c.bufferSize = 0
+	}
 
 	return c
 }
 
+// SetMaxBackups 设置 rotate 后的 log 文件数量。
 func (c *Config) SetMaxBackups(maxBackups int) *Config {
-	c.logMaxBackups = maxBackups
+	c.maxBackups = maxBackups
 
 	return c
 }
 
+// SetCompress 设置是否压缩 rotate 后的 log 文件。默认为 false。
 func (c *Config) SetCompress(compress bool) *Config {
-	c.logCompress = compress
+	c.compress = compress
 
 	return c
 }
 
+// SetTimeFormat 设置 log 里记录的时间格式。默认为 time.RFC3339。
 func (c *Config) SetTimeFormat(timeFormat string) *Config {
-	c.logTimeFormat = timeFormat
+	c.timeFormat = timeFormat
 
 	return c
 }
--- logger/logger.go
+++ logger/logger.go
@@ -17,7 +17,7 @@ func New(c *Config) (logger slog.SLogger, err error) {
 	var logTemplate string
 	var syslogLevel syslog.Priority
 
-	level := slog.LevelByName(c.logLevel)
+	level := slog.LevelByName(c.level)
 	if level == slog.DebugLevel {
 		syslogLevel = syslog.LOG_DEBUG
 		// 当 log level 为 debug 时开启 caller，方便快速定位打印日志位置
@@ -30,26 +30,27 @@ func New(c *Config) (logger slog.SLogger, err error) {
 
 	// custom log format
 	logFormatter := slog.NewTextFormatter(logTemplate)
-	logFormatter.TimeFormat = c.logTimeFormat
-	l := slog.NewStdLogger().Configure(func(sl *slog.SugaredLogger) {
-		sl.ReportCaller = true
-		sl.CallerSkip = 6
-	})
+	logFormatter.TimeFormat = c.timeFormat
+
+	l := slog.NewStdLogger()
+	l.ReportCaller = true
+	l.CallerSkip = 6
+
 	l.Config(func(sl *slog.SugaredLogger) {
 		f := sl.Formatter.(*slog.TextFormatter)
-		f.TimeFormat = c.logTimeFormat
+		f.TimeFormat = c.timeFormat
 		f.SetTemplate(logTemplate)
 		f.FullDisplay = true
 		f.EnableColor = false
 	})
 
-	switch c.logTarget {
+	switch c.target {
 	case "stdout":
 		h := handler.NewConsoleHandler([]slog.Level{level})
 		h.SetFormatter(logFormatter)
 		l.AddHandler(h)
 	case "file":
-		if c.logMaxSize > 0 {
+		if c.maxSize > 0 {
 			h, err := handlerRotateFile(c)
 			if err != nil {
 				return nil, err
@@ -59,7 +60,7 @@ func New(c *Config) (logger slog.SLogger, err error) {
 			l.AddHandler(h)
 		}
 
-		if c.logRotateInterval != "" {
+		if c.rotateInterval != "" {
 			h, err := handlerRotateTime(c)
 			if err != nil {
 				return nil, err
@@ -69,13 +70,13 @@ func New(c *Config) (logger slog.SLogger, err error) {
 			l.AddHandler(h)
 		}
 	case "syslog":
-		if len(c.logSyslogServer) == 0 {
+		if len(c.syslogServer) == 0 {
 			// Use local syslog socket by default.
-			c.logSyslogServer = "/dev/log"
+			c.syslogServer = "/dev/log"
 		}
 
-		if strings.HasPrefix(c.logSyslogServer, "/") {
-			h, err := handler.NewSysLogHandler(syslogLevel|syslog.LOG_MAIL, c.logSyslogTag)
+		if strings.HasPrefix(c.syslogServer, "/") {
+			h, err := handler.NewSysLogHandler(syslogLevel|syslog.LOG_MAIL, c.syslogTag)
 			if err != nil {
 				return nil, err
 			}
@@ -85,11 +86,11 @@ func New(c *Config) (logger slog.SLogger, err error) {
 			break
 		}
 
-		w, err := syslog.Dial("tcp", c.logSyslogServer, syslogLevel|syslog.LOG_MAIL, c.logSyslogTag)
+		w, err := syslog.Dial("tcp", c.syslogServer, syslogLevel|syslog.LOG_MAIL, c.syslogTag)
 		if err != nil {
 			return nil, err
 		}
-		h := handler.NewBufferedHandler(w, c.logBufferSize, level)
+		h := handler.NewBufferedHandler(w, c.bufferSize, level)
 		h.SetFormatter(logFormatter)
 		l.AddHandler(h)
 	}
@@ -104,9 +105,9 @@ func New(c *Config) (logger slog.SLogger, err error) {
 func handlerRotateFile(c *Config) (*handler.SyncCloseHandler, error) {
 	return handler.NewSizeRotateFileHandler(
 		c.logFile,
-		c.logMaxSize,
-		handler.WithBuffSize(c.logBufferSize),
-		handler.WithCompress(c.logCompress),
+		c.maxSize,
+		handler.WithBuffSize(c.bufferSize),
+		handler.WithCompress(c.compress),
 		handler.WithLogLevels(slog.AllLevels),
 	)
 }
@@ -114,17 +115,17 @@ func handlerRotateFile(c *Config) (*handler.SyncCloseHandler, error) {
 // handlerRotateTime
 // rotateInterval: 1w, 1d, 1h, 1m, 1s
 func handlerRotateTime(c *Config) (*handler.SyncCloseHandler, error) {
-	if len(c.logRotateInterval) == 0 {
+	if len(c.rotateInterval) == 0 {
 		return nil, errors.New("empty rotate interval")
 	}
 
-	lastChar := c.logRotateInterval[len(c.logRotateInterval)-1]
+	lastChar := c.rotateInterval[len(c.rotateInterval)-1]
 	lowerLastChar := strings.ToLower(string(lastChar))
 	if !slices.Contains([]string{"w", "d", "h", "m", "s"}, lowerLastChar) {
 		return nil, fmt.Errorf("unsuppored rotate interval type: %s", lowerLastChar)
 	}
 
-	rotateIntervalDuration, err := time.ParseDuration(c.logRotateInterval)
+	rotateIntervalDuration, err := time.ParseDuration(c.rotateInterval)
 	if err != nil {
 		return nil, err
 	}
@@ -132,8 +133,8 @@ func handlerRotateTime(c *Config) (*handler.SyncCloseHandler, error) {
 	return handler.NewTimeRotateFileHandler(
 		c.logFile,
 		rotatefile.RotateTime(rotateIntervalDuration.Seconds()),
-		handler.WithBuffSize(c.logBufferSize),
-		handler.WithCompress(c.logCompress),
+		handler.WithBuffSize(c.bufferSize),
+		handler.WithCompress(c.compress),
 		handler.WithLogLevels(slog.AllLevels),
 	)
 }
