15df9362ed83ed58bf5a4fbdfb06f8dc44792fe5
ly020044 <ly020044@gmail.com>
2023-06-21 16:19:32 +0800

    Update doveadmpw

--- doveadmpw/doveadmpw.go
+++ doveadmpw/doveadmpw.go
@@ -41,3 +41,36 @@ func VerifyPassword(hash, plain string) (matched bool, err error) {
 
 	return
 }
+
+func IsSupportedPasswordScheme(pwHash string) bool {
+	if !(strings.HasPrefix(pwHash, "{") && strings.Contains(pwHash, "}")) {
+		return false
+	}
+
+	// Extract scheme name from password hash: "{SSHA}xxxx" -> "SSHA"
+	scheme := strings.Split(strings.Split(pwHash, "}")[0], "{")[1]
+	scheme = strings.ToUpper(scheme)
+
+	supportedSchemes := []string{
+		"PLAIN",
+		"CRYPT",
+		"MD5",
+		"PLAIN-MD5",
+		"SHA",
+		"SSHA",
+		"SHA512",
+		"SSHA512",
+		"SHA512-CRYPT",
+		"BCRYPT",
+		"CRAM-MD5",
+		"NTLM",
+	}
+
+	for _, supportedScheme := range supportedSchemes {
+		if scheme == supportedScheme {
+			return true
+		}
+	}
+
+	return false
+}
