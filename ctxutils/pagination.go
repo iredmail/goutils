ad5ea07061cf3912b10337c611c638ccb03a45c6
Zhang Huangbin <zhb@iredmail.org>
2023-07-13 20:49:03 +0800

    Simplify ctxutils.GenPagination().

--- ctxutils/pagination.go
+++ ctxutils/pagination.go
@@ -4,22 +4,23 @@ import (
 	"math"
 )
 
-// PageLink 表示一个分页链接。
-type PageLink struct {
-	Page   int
-	Active bool
-}
-
-// pageLink 将页数转化为结构体 PageLink。
-func pageLink(currentPage, num int) PageLink {
-	return PageLink{
-		Page:   num,
-		Active: currentPage == num,
-	}
+type Pagination struct {
+	TotalItems  int64
+	TotalPages  int
+	CurrentPage int
+	PageNumbers []int // 数字为 0 表示省略的范围，可以以省略号表示。
 }
 
 // GenPagination 根据当前页 `page`，总条目数 `total`，每页条目数 `limit` 生成分页链接。
-func GenPagination(page int, total int64, limit int) (pl []PageLink) {
+func GenPagination(page int, total int64, limit int) (p Pagination) {
+	p = Pagination{
+		TotalItems:  total,
+		TotalPages:  int(math.Ceil(float64(total) / float64(limit))),
+		CurrentPage: page,
+	}
+
+	var nums []int
+
 	if total == 0 {
 		return
 	}
@@ -34,7 +35,7 @@ func GenPagination(page int, total int64, limit int) (pl []PageLink) {
 	if pages <= 10 {
 		// 10 页以内直接全部显示
 		for i := 1; i <= pages; i++ {
-			pl = append(pl, pageLink(page, i))
+			nums = append(nums, i)
 		}
 	} else {
 		//
@@ -42,54 +43,56 @@ func GenPagination(page int, total int64, limit int) (pl []PageLink) {
 		//
 
 		// 第一页
-		pl = append(pl, pageLink(page, 1))
+		nums = append(nums, 1)
 
 		if page <= 4 {
 			// 前面几页全部显示
 			for i := 2; i <= page; i++ {
-				pl = append(pl, pageLink(page, i))
+				nums = append(nums, i)
 			}
 
 			// 后面3页及最后一页
-			pl = append(pl,
-				pageLink(page, page+1),
-				pageLink(page, page+2),
-				pageLink(page, page+3),
-				PageLink{Page: 0},     // 省略号
-				pageLink(page, pages), // 最后一页
+			nums = append(nums,
+				page+1,
+				page+2,
+				page+3,
+				0,     // 省略号
+				pages, // 最后一页
 			)
 		} else if page > 4 && page < pages-4 {
 			// 添加当前页及其前后3页
-			pl = append(pl,
-				PageLink{Page: 0}, // 省略号
-				pageLink(page, page-3),
-				pageLink(page, page-2),
-				pageLink(page, page-1),
-				pageLink(page, page), // 当前页
-				pageLink(page, page+1),
-				pageLink(page, page+2),
-				pageLink(page, page+3),
-				PageLink{Page: 0},     // 省略号
-				pageLink(page, pages), // 最后一页
+			nums = append(nums,
+				0, // 省略号
+				page-3,
+				page-2,
+				page-1,
+				page, // 当前页
+				page+1,
+				page+2,
+				page+3,
+				0,     // 省略号
+				pages, // 最后一页
 			)
 		} else {
 			// 前面添加一个省略号（不带链接）
 
 			// 当前页及其前面3页
-			pl = append(pl,
-				PageLink{Page: 0},
-				pageLink(page, page-3),
-				pageLink(page, page-2),
-				pageLink(page, page-1),
-				pageLink(page, page), // 当前页
+			nums = append(nums,
+				0,
+				page-3,
+				page-2,
+				page-1,
+				page, // 当前页
 			)
 
 			// 最后几页全部显示
 			for i := page + 1; i <= pages; i++ {
-				pl = append(pl, pageLink(page, i))
+				nums = append(nums, i)
 			}
 		}
 	}
+
+	p.PageNumbers = nums
 
 	return
 }
