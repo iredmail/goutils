ca0e6f4c4c12b39f6e652721b4cb80c27f67fe8c
Zhang Huangbin <zhb@iredmail.org>
2023-07-21 20:09:55 +0800

    获取 sqlite 数据库的 auto_vacuum 设置。

--- sqlutils/db_stat.go
+++ sqlutils/db_stat.go
@@ -10,20 +10,33 @@ type DBStat struct {
 
 	// Pragma
 	JournalMode string // 日志模式
+	AutoVacuum  string
 }
 
 // GetDBStat 返回指定数据库的相关信息。
 func GetDBStat(pth string, db *goqu.Database) (stat DBStat) {
 	var pageSize, pageCount int64
-	var journalMode string
+	var journalMode, autoVacuum string
 
 	_ = db.QueryRow("PRAGMA page_size").Scan(&pageSize)
 	_ = db.QueryRow("PRAGMA page_count").Scan(&pageCount)
 	_ = db.QueryRow("PRAGMA journal_mode").Scan(&journalMode)
 
+	_ = db.QueryRow("PRAGMA auto_vacuum").Scan(&autoVacuum)
+	switch autoVacuum {
+	case "0":
+		autoVacuum = "none"
+	case "1":
+		autoVacuum = "full"
+	case "2":
+		autoVacuum = "incremental"
+	}
+
 	return DBStat{
-		Path:        pth,
-		Size:        pageSize * pageCount,
+		Path: pth,
+		Size: pageSize * pageCount,
+
 		JournalMode: journalMode,
+		AutoVacuum:  autoVacuum,
 	}
 }
