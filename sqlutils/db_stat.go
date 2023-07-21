a895d90918c74719fb216bf8ab1dee3568ef08b0
Zhang Huangbin <zhb@iredmail.org>
2023-07-21 20:04:38 +0800

    获取 sqlite 数据库的 journal mode 设置。

--- sqlutils/db_stat.go
+++ sqlutils/db_stat.go
@@ -7,17 +7,23 @@ import (
 type DBStat struct {
 	Path string // 数据库名称或文件路径
 	Size int64
+
+	// Pragma
+	JournalMode string // 日志模式
 }
 
 // GetDBStat 返回指定数据库的相关信息。
 func GetDBStat(pth string, db *goqu.Database) (stat DBStat) {
 	var pageSize, pageCount int64
+	var journalMode string
 
 	_ = db.QueryRow("PRAGMA page_size").Scan(&pageSize)
 	_ = db.QueryRow("PRAGMA page_count").Scan(&pageCount)
+	_ = db.QueryRow("PRAGMA journal_mode").Scan(&journalMode)
 
 	return DBStat{
-		Path: pth,
-		Size: pageSize * pageCount,
+		Path:        pth,
+		Size:        pageSize * pageCount,
+		JournalMode: journalMode,
 	}
 }
