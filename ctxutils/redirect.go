0292007d270c45d582a4b7a2a4d9e450dec9d33f
Zhang Huangbin <zhb@iredmail.org>
2023-07-24 22:46:00 +0800

    handle multiple query parameters.

--- ctxutils/redirect.go
+++ ctxutils/redirect.go
@@ -1,17 +1,27 @@
 package ctxutils
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 )
 
+func addQueryMark(uri string) (newURI string) {
+	if strings.Contains(uri, "?") {
+		return uri + "&"
+	} else {
+		return uri + "?"
+	}
+}
+
 func RedirectWithString(ctx *fiber.Ctx, uri, msg string) error {
 	// TODO URI 是否需要添加 HomePath 前缀？
-	return ctx.Redirect(uri + "?msg=" + msg)
+	return ctx.Redirect(addQueryMark(uri) + "msg=" + msg)
 }
 
 func RedirectWithError(ctx *fiber.Ctx, uri string, err error) error {
 	if err != nil {
-		return ctx.Redirect(uri + "?msg=" + err.Error())
+		return ctx.Redirect(addQueryMark(uri) + "msg=" + err.Error())
 	}
 
 	// TODO URI 是否需要添加 HomePath 前缀？
