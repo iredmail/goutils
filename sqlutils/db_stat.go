39b8c85f82b9ad8f291fb2958bae31c6f56b9134
Zhang Huangbin <zhb@iredmail.org>
2023-07-31 13:20:41 +0800

    New struct `TableStat`, func `GetTableStats()`.

--- sqlutils/db_stat.go
+++ sqlutils/db_stat.go
@@ -16,6 +16,14 @@ type DBStat struct {
 	Synchronous string
 }
 
+type TableStat struct {
+	// Table name.
+	Name string `db:"name"`
+
+	// Table size in bytes
+	Size int64 `db:"table_size"`
+}
+
 // GetDBStat 返回指定数据库的相关信息。
 func GetDBStat(pth string, db *goqu.Database) (stat DBStat) {
 	var pageSize, pageCount int64
@@ -56,4 +64,18 @@ func GetDBStat(pth string, db *goqu.Database) (stat DBStat) {
 		AutoVacuum:  strings.ToUpper(autoVacuum),
 		Synchronous: strings.ToUpper(synchronous),
 	}
+}
+
+// GetTableStats 返回数据库中所有表的大小，包含索引。
+// FYI <https://www.sqlite.org/dbstat.html>
+func GetTableStats(db *goqu.Database) (rows []TableStat, err error) {
+	// SELECT name ,SUM(pgsize)/1024 table_size FROM "dbstat" GROUP BY name ORDER BY table_size desc;
+	err = db.
+		Select("name", "SUM(pgsize) table_size").
+		From("dbstat").
+		GroupBy("name").
+		Order(goqu.C("table_size").Desc()).
+		ScanStructs(&rows)
+
+	return
 }
