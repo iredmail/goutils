f77e632a136a684fdae10c1d1bd058798d3359bf
ly020044 <ly020044@gmail.com>
2023-09-13 08:58:24 +0800

    更新 sslcert.WithCertDomain 方法，检测传入的域名是否有效

--- sslcert/option.go
+++ sslcert/option.go
@@ -1,10 +1,18 @@
 package sslcert
 
+import (
+	"github.com/iredmail/goutils/emailutils"
+)
+
 type Option func(m *Manager)
 
 func WithCertDomain(domains ...string) Option {
 	return func(m *Manager) {
-		m.certDomains = domains
+		for _, domain := range domains {
+			if emailutils.IsDomain(domain) {
+				m.certDomains = append(m.certDomains, domain)
+			}
+		}
 	}
 }
 
