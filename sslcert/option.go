b8524bb941d2e4ac1dd5a111d27e3b3c3be41b52
ly020044 <ly020044@gmail.com>
2023-09-13 09:06:17 +0800

    Fix last commit.

--- sslcert/option.go
+++ sslcert/option.go
@@ -1,6 +1,8 @@
 package sslcert
 
 import (
+	"strings"
+
 	"github.com/iredmail/goutils/emailutils"
 )
 
@@ -9,6 +11,7 @@ type Option func(m *Manager)
 func WithCertDomain(domains ...string) Option {
 	return func(m *Manager) {
 		for _, domain := range domains {
+			domain = strings.ToLower(domain)
 			if emailutils.IsDomain(domain) {
 				m.certDomains = append(m.certDomains, domain)
 			}
