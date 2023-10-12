e63c330669fa188b9c128392302437ccbfbc610c
Zhang Huangbin <zhb@iredmail.org>
2023-10-12 21:32:04 +0800

    微调 smtpclient。

---
+++ smtpclient/composer.go
@@ -0,0 +1,160 @@
+package smtpclient
+
+import (
+	"bytes"
+	"net/mail"
+	"time"
+
+	"github.com/jhillyerd/enmime"
+
+	"github.com/iredmail/goutils"
+)
+
+type Composer struct {
+	From     mail.Address
+	To       []mail.Address
+	Subject  string // mail subject
+	BodyText []byte // mail body in plain text format
+	BodyHTML []byte // mail body in html format
+
+	// Optional
+	cc              []mail.Address // header `Cc:`
+	bcc             []mail.Address // header `Bcc:`
+	replyTo         mail.Address   // header `Reply-To:`
+	date            time.Time      // header `Date:`
+	messageID       string         // header `Message-ID:`
+	headers         map[string]string
+	fileAttachments []string // Path to files
+	byteAttachments []*ByteAttachment
+}
+
+type ByteAttachment struct {
+	Name        string
+	ContentType string
+	Bytes       []byte
+}
+
+func (c *Composer) WithCc(cc []mail.Address) *Composer {
+	c.cc = cc
+
+	return c
+}
+
+func (c *Composer) WithBcc(bcc []mail.Address) *Composer {
+	c.bcc = bcc
+
+	return c
+}
+
+func (c *Composer) WithReplyTo(replyTo mail.Address) *Composer {
+	c.replyTo = replyTo
+
+	return c
+}
+
+func (c *Composer) WithDate(t time.Time) *Composer {
+	c.date = t
+
+	return c
+
+}
+
+func (c *Composer) WithMessageID(msgid string) *Composer {
+	c.messageID = msgid
+
+	return c
+
+}
+
+func (c *Composer) WithHeaders(headers map[string]string) *Composer {
+	c.headers = headers
+
+	return c
+
+}
+
+func (c *Composer) WithFileAttachments(pths ...string) *Composer {
+	c.fileAttachments = pths
+
+	return c
+}
+
+func (c *Composer) WithByteAttachments(atts ...*ByteAttachment) *Composer {
+	c.byteAttachments = atts
+
+	return c
+}
+
+// Bytes 将邮件内容转换为 `[]byte`。
+func (c *Composer) Bytes() (msg []byte, err error) {
+	mb := enmime.Builder().
+		From(c.From.Name, c.From.Address).
+		ToAddrs(c.To).
+		Subject(c.Subject)
+
+	if len(c.BodyText) > 0 {
+		mb = mb.Text(c.BodyText)
+	}
+
+	if len(c.BodyHTML) > 0 {
+		mb = mb.HTML(c.BodyHTML)
+	}
+
+	if c.cc != nil {
+		mb = mb.CCAddrs(c.cc)
+	}
+
+	if c.cc != nil {
+		mb = mb.CCAddrs(c.cc)
+	}
+
+	if len(c.replyTo.Address) > 0 {
+		mb = mb.ReplyTo(c.replyTo.Name, c.replyTo.Address)
+	}
+
+	if c.date.UTC().Unix() > 0 {
+		mb = mb.Date(c.date)
+	} else {
+		mb = mb.Date(time.Now().UTC())
+	}
+
+	if len(c.messageID) > 0 {
+		c.headers["Message-ID"] = c.messageID
+	} else {
+		// Use random Message-ID
+		c.headers["Message-ID"] = goutils.GenRandomString(32) + "@" + goutils.GetHostFQDN()
+	}
+
+	if c.headers != nil {
+		for k, v := range c.headers {
+			mb = mb.Header(k, v)
+		}
+	}
+
+	if c.fileAttachments != nil {
+		for _, pth := range c.fileAttachments {
+			mb = mb.AddFileAttachment(pth)
+		}
+	}
+
+	if c.byteAttachments != nil {
+		for _, att := range c.byteAttachments {
+			mb = mb.AddAttachment(att.Bytes, att.ContentType, att.Name)
+		}
+	}
+
+	part, err := mb.Build()
+	if err != nil {
+		return
+	}
+
+	var buf bytes.Buffer
+	err = part.Encode(&buf)
+	if err != nil {
+		return
+	}
+
+	msg = buf.Bytes()
+
+	return
+}
--- smtpclient/smtp.go
+++ smtpclient/smtp.go
@@ -3,6 +3,8 @@ package smtpclient
 import (
 	"crypto/tls"
 	"errors"
+	"fmt"
+	"log"
 	"net"
 	"net/mail"
 	"net/smtp"
@@ -11,94 +13,186 @@ import (
 	"time"
 
 	"github.com/iredmail/goutils"
+	"github.com/iredmail/goutils/emailutils"
 )
 
 type Config struct {
-	Host     string
-	Port     string
+	Host string
+	Port string
+
 	StartTLS bool
-	Timeout  time.Duration
+	// TODO skip ssl cert verification
+
+	// Sender
+	From mail.Address
 
 	// smtp authentication
 	SMTPUser     string
 	SMTPPassword string
 }
 
-func Sendmail(c Config, from string, to []string, subject string, body []byte, builder ...Builder) error {
-	msg := message{
-		from:    from,
-		to:      to,
-		subject: subject,
-		body:    body,
+func SendmailWithComposer(c Config, composer *Composer) (err error) {
+	// Export mail body before smtp connection, make sure it's valid email message.
+	msg, err := composer.Bytes()
+	if err != nil {
+		return fmt.Errorf("invalid email message: %v", err)
 	}
 
-	for _, build := range builder {
-		build(&msg)
-	}
-
-	encodeMsg, err := msg.Encode(c.Host)
+	client, err := smtp.Dial(net.JoinHostPort(c.Host, c.Port))
 	if err != nil {
 		return err
 	}
 
-	if c.Timeout == 0 {
-		c.Timeout = time.Second * 10
-	}
-
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort(c.Host, c.Port), c.Timeout)
-	if err != nil {
-		return err
-	}
-
-	client, err := smtp.NewClient(conn, c.Host)
-	if err != nil {
-		return err
-	}
-
-	defer func(client *smtp.Client) {
-		_ = client.Quit()
-	}(client)
-
-	if err = client.Hello(goutils.GetHostFQDN()); err != nil {
-		return err
-	}
-
-	hasExtSTARTTLS, _ := client.Extension("STARTTLS")
-	if !hasExtSTARTTLS && c.StartTLS {
-		return errors.New("smtp server unsupported tls")
-	}
-
-	if hasExtSTARTTLS && c.StartTLS {
-		if err = client.StartTLS(&tls.Config{
+	if c.StartTLS {
+		tc := &tls.Config{
 			InsecureSkipVerify: true,
 			ServerName:         c.Host,
-		}); err != nil {
+		}
+
+		if err = client.StartTLS(tc); err != nil {
 			return err
 		}
 	}
 
 	if len(c.SMTPUser) > 0 && len(c.SMTPPassword) > 0 {
-		var auth smtp.Auth
-
-		if ok, auths := client.Extension("AUTH"); ok {
-			if strings.Contains(auths, "LOGIN") &&
-				!strings.Contains(auths, "PLAIN") {
-				auth = newLoginAuth(c.Host, c.SMTPUser, c.SMTPPassword)
-			} else {
-				auth = smtp.PlainAuth("", c.SMTPUser, c.SMTPPassword, c.Host)
-			}
-		}
+		auth := smtp.PlainAuth("", c.SMTPUser, c.SMTPPassword, c.Host)
 
 		if err = client.Auth(auth); err != nil {
 			return err
 		}
 	}
 
-	if err = client.Mail(from); err != nil {
+	if err = client.Mail(c.From.Address); err != nil {
 		return err
 	}
 
-	for _, rcpt := range to {
+	for _, addr := range composer.To {
+		if err = client.Rcpt(addr.Address); err != nil {
+			return err
+		}
+	}
+
+	for _, addr := range composer.cc {
+		if err = client.Rcpt(addr.Address); err != nil {
+			return err
+		}
+	}
+
+	for _, addr := range composer.bcc {
+		if err = client.Rcpt(addr.Address); err != nil {
+			return err
+		}
+	}
+
+	w, err := client.Data()
+	if err != nil {
+		return err
+	}
+
+	if _, err = w.Write(msg); err != nil {
+		log.Fatalln(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	return client.Quit()
+}
+
+// SendmailWithComposerInBackground 在后台发送邮件，不阻塞当前进程。
+func SendmailWithComposerInBackground(c Config, composer *Composer) {
+	go func() {
+		_ = SendmailWithComposer(c, composer)
+	}()
+}
+
+// Sendmail 发送邮件。
+// 简化邮件格式。
+func Sendmail(c Config, recipients, bcc []string, replyTo, subject, body string) error {
+	// 过滤出有效的邮件地址
+	var rcptAddrs []string
+	for _, rcpt := range recipients {
+		if emailutils.IsEmail(rcpt) {
+			rcptAddrs = append(rcptAddrs, rcpt)
+		}
+	}
+
+	if len(rcptAddrs) == 0 {
+		return errors.New("invalid recipients")
+	}
+
+	// 构造邮件头
+	headers := map[string]string{
+		"From":       c.From.String(),
+		"To":         strings.Join(rcptAddrs, ","),
+		"Subject":    subject,
+		"Message-ID": fmt.Sprintf("<%s@%s>", goutils.GenRandomString(32), c.Host),
+		"Date":       time.Now().UTC().Format(time.RFC1123Z),
+	}
+
+	if emailutils.IsEmail(replyTo) {
+		headers["Reply-To"] = replyTo
+	}
+
+	if len(bcc) > 0 {
+		var bccAddrs []string
+
+		for _, addr := range bcc {
+			bccAddrs = append(bccAddrs, addr)
+		}
+
+		headers["Bcc"] = strings.Join(bccAddrs, ",")
+	}
+
+	message := ""
+	for k, v := range headers {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+
+	// 邮件 header 和 body 以第一个空白行作为分界
+	message += "\r\n" + body
+
+	client, err := smtp.Dial(net.JoinHostPort(c.Host, c.Port))
+	if err != nil {
+		return err
+	}
+
+	err = client.Hello(goutils.GetHostFQDN())
+	if err != nil {
+		return err
+	}
+
+	if c.StartTLS {
+		tc := &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         c.Host,
+		}
+
+		if err = client.StartTLS(tc); err != nil {
+			return err
+		}
+
+		err = client.Hello(goutils.GetHostFQDN())
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(c.SMTPUser) > 0 && len(c.SMTPPassword) > 0 {
+		auth := smtp.PlainAuth("", c.SMTPUser, c.SMTPPassword, c.Host)
+
+		if err = client.Auth(auth); err != nil {
+			return err
+		}
+	}
+
+	if err = client.Mail(c.From.Address); err != nil {
+		return err
+	}
+
+	for _, rcpt := range rcptAddrs {
 		if err = client.Rcpt(rcpt); err != nil {
 			return err
 		}
@@ -109,98 +203,99 @@ func Sendmail(c Config, from string, to []string, subject string, body []byte, b
 		return err
 	}
 
-	if _, err = w.Write(encodeMsg); err != nil {
+	if _, err = w.Write([]byte(message)); err != nil {
+		log.Fatalln(err)
+	}
+
+	err = w.Close()
+	if err != nil {
 		return err
 	}
 
-	return w.Close()
+	return client.Quit()
 }
 
-func SendmailInBackground(c Config, from string, to []string, subject string, body []byte, builder ...Builder) {
+// SendmailInBackground 在后台发送邮件，不阻塞当前进程。
+func SendmailInBackground(c Config, recipients, bcc []string, replyTo, subject, body string) {
 	go func() {
 		// TODO return error
-		_ = Sendmail(c, from, to, subject, body, builder...)
+		_ = Sendmail(c, recipients, bcc, replyTo, subject, body)
 	}()
 }
 
-func SendmailWithEml(c Config, from mail.Address, to []string, emlPath string) error {
+func SendmailWithEml(c Config, from mail.Address, recipients []string, emlPath string) error {
+	smtpServer := fmt.Sprintf("%s:%s", c.Host, c.Port)
+
+	// CONNECT
+	client, err := smtp.Dial(smtpServer)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = client.Close() }()
+
+	// HELO
+	domain := emailutils.ExtractDomain(from.Address)
+	if domain == "" {
+		domain = "example.com"
+	}
+	if err = client.Hello(domain); err != nil {
+		return err
+	}
+
+	// TLS
+	if c.StartTLS {
+		tlsConfig := &tls.Config{
+			// ServerName:         utils.GetHostName(),
+			InsecureSkipVerify: true,
+		}
+		err = client.StartTLS(tlsConfig)
+		if err != nil {
+			fmt.Printf("failed in STARTTLS directive: %v\n", err)
+			os.Exit(255)
+		}
+	}
+
+	auth := smtp.PlainAuth("", c.SMTPUser, c.SMTPPassword, c.Host)
+	if err = client.Auth(auth); err != nil {
+		return err
+	}
+
+	// `MAIL FROM:`
+	if err = client.Mail(from.Address); err != nil {
+		return err
+	}
+
+	// `RCPT TO:`
+	var toAddrs []string
+	for _, addr := range recipients {
+		toAddrs = append(toAddrs, addr)
+	}
+	to := strings.Join(toAddrs, ",")
+	if err = client.Rcpt(to); err != nil {
+		return err
+	}
+
+	// `DATA`
+	w, err := client.Data()
+	if err != nil {
+		return err
+	}
+
 	// 读取邮件源码文件
 	rawEmail, err := os.ReadFile(emlPath)
 	if err != nil {
 		return err
 	}
 
-	if c.Timeout == 0 {
-		c.Timeout = time.Second * 10
-	}
-
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort(c.Host, c.Port), c.Timeout)
+	_, err = w.Write(rawEmail)
 	if err != nil {
 		return err
 	}
 
-	client, err := smtp.NewClient(conn, c.Host)
+	err = w.Close()
 	if err != nil {
 		return err
 	}
 
-	defer func(client *smtp.Client) {
-		_ = client.Quit()
-	}(client)
-
-	if err = client.Hello(goutils.GetHostFQDN()); err != nil {
-		return err
-	}
-
-	hasExtSTARTTLS, _ := client.Extension("STARTTLS")
-	if !hasExtSTARTTLS && c.StartTLS {
-		return errors.New("smtp server unsupported tls")
-	}
-
-	if hasExtSTARTTLS && c.StartTLS {
-		if err = client.StartTLS(&tls.Config{
-			InsecureSkipVerify: true,
-			ServerName:         c.Host,
-		}); err != nil {
-			return err
-		}
-	}
-
-	if len(c.SMTPUser) > 0 && len(c.SMTPPassword) > 0 {
-		var auth smtp.Auth
-
-		if ok, auths := client.Extension("AUTH"); ok {
-			if strings.Contains(auths, "LOGIN") &&
-				!strings.Contains(auths, "PLAIN") {
-				auth = newLoginAuth(c.Host, c.SMTPUser, c.SMTPPassword)
-			} else {
-				auth = smtp.PlainAuth("", c.SMTPUser, c.SMTPPassword, c.Host)
-			}
-		}
-
-		if err = client.Auth(auth); err != nil {
-			return err
-		}
-	}
-
-	if err = client.Mail(from.Address); err != nil {
-		return err
-	}
-
-	for _, rcpt := range to {
-		if err = client.Rcpt(rcpt); err != nil {
-			return err
-		}
-	}
-
-	w, err := client.Data()
-	if err != nil {
-		return err
-	}
-
-	if _, err = w.Write(rawEmail); err != nil {
-		return err
-	}
-
-	return w.Close()
+	return client.Quit()
 }
