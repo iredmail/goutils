5e8294d692369915b92a200649bc3d5d007d3c0b
ly020044 <ly020044@gmail.com>
2023-10-12 10:03:04 +0800

    Update package smtpclient.

--- smtpclient/smtp.go
+++ smtpclient/smtp.go
@@ -12,7 +12,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/iredmail/goutils"
 	"github.com/iredmail/goutils/emailutils"
 )
 
@@ -20,90 +19,86 @@ type Config struct {
 	Host     string
 	Port     string
 	StartTLS bool
-
-	// Sender
-	From mail.Address
+	Timeout  time.Duration
 
 	// smtp authentication
 	SMTPUser     string
 	SMTPPassword string
 }
 
-func Sendmail(c Config, recipients, bcc []string, replyTo, subject, body string) error {
-	// 过滤出有效的邮件地址
-	var rcptAddrs []string
-	for _, rcpt := range recipients {
-		if emailutils.IsEmail(rcpt) {
-			rcptAddrs = append(rcptAddrs, rcpt)
-		}
+func Sendmail(c Config, from string, to []string, subject string, body []byte, builder ...Builder) error {
+	msg := message{
+		from:    from,
+		to:      to,
+		subject: subject,
+		body:    body,
 	}
 
-	if len(rcptAddrs) == 0 {
-		return errors.New("invalid recipients")
+	for _, build := range builder {
+		build(&msg)
 	}
 
-	rcpts := strings.Join(rcptAddrs, ",")
-
-	// 构造邮件头
-	headers := map[string]string{
-		"From":       c.From.String(),
-		"To":         rcpts,
-		"Subject":    subject,
-		"Message-ID": fmt.Sprintf("<%s@%s>", goutils.GenRandomString(32), c.Host),
-		"Date":       time.Now().UTC().Format(time.RFC1123Z),
-	}
-
-	if emailutils.IsEmail(replyTo) {
-		headers["Reply-To"] = replyTo
-	}
-
-	if len(bcc) > 0 {
-		var bccAddrs []string
-
-		for _, addr := range bcc {
-			bccAddrs = append(bccAddrs, addr)
-		}
-
-		headers["Bcc"] = strings.Join(bccAddrs, ",")
-	}
-
-	message := ""
-	for k, v := range headers {
-		message += fmt.Sprintf("%s: %s\r\n", k, v)
-	}
-
-	// 邮件 header 和 body 以第一个空白行作为分界
-	message += "\r\n" + body
-
-	client, err := smtp.Dial(net.JoinHostPort(c.Host, c.Port))
+	encodeMsg, err := msg.Encode(c.Host)
 	if err != nil {
 		return err
 	}
 
-	if c.StartTLS {
-		tc := &tls.Config{
+	if c.Timeout == 0 {
+		c.Timeout = time.Second * 10
+	}
+
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(c.Host, c.Port), c.Timeout)
+	if err != nil {
+		return err
+	}
+
+	client, err := smtp.NewClient(conn, c.Host)
+	if err != nil {
+		return err
+	}
+
+	if err = client.Hello(emailutils.ExtractDomain(from)); err != nil {
+		return err
+	}
+
+	hasExtSTARTTLS, _ := client.Extension("STARTTLS")
+	if !hasExtSTARTTLS && c.StartTLS {
+		return errors.New("smtp server unsupported tls")
+	}
+
+	if hasExtSTARTTLS && c.StartTLS {
+		if err = client.StartTLS(&tls.Config{
 			InsecureSkipVerify: true,
 			ServerName:         c.Host,
-		}
-
-		if err = client.StartTLS(tc); err != nil {
+		}); err != nil {
 			return err
 		}
 	}
 
 	if len(c.SMTPUser) > 0 && len(c.SMTPPassword) > 0 {
-		auth := smtp.PlainAuth("", c.SMTPUser, c.SMTPPassword, c.Host)
+		var auth smtp.Auth
+
+		if ok, auths := client.Extension("AUTH"); ok {
+			if strings.Contains(auths, "CRAM-MD5") {
+				auth = smtp.CRAMMD5Auth(c.SMTPUser, c.SMTPPassword)
+			} else if strings.Contains(auths, "LOGIN") &&
+				!strings.Contains(auths, "PLAIN") {
+				auth = newLoginAuth(c.Host, c.SMTPUser, c.SMTPPassword)
+			} else {
+				auth = smtp.PlainAuth("", c.SMTPUser, c.SMTPPassword, c.Host)
+			}
+		}
 
 		if err = client.Auth(auth); err != nil {
 			return err
 		}
 	}
 
-	if err = client.Mail(c.From.Address); err != nil {
+	if err = client.Mail(from); err != nil {
 		return err
 	}
 
-	if err = client.Rcpt(rcpts); err != nil {
+	if err = client.Rcpt(strings.Join(to, ",")); err != nil {
 		return err
 	}
 
@@ -112,22 +107,21 @@ func Sendmail(c Config, recipients, bcc []string, replyTo, subject, body string)
 		return err
 	}
 
-	if _, err = w.Write([]byte(message)); err != nil {
+	if _, err = w.Write(encodeMsg); err != nil {
 		log.Fatalln(err)
 	}
 
-	err = w.Close()
-	if err != nil {
+	if err = w.Close(); err != nil {
 		return err
 	}
 
 	return client.Quit()
 }
 
-func SendmailInBackground(c Config, recipients, bcc []string, replyTo, subject, body string) {
+func SendmailInBackground(c Config, from string, to []string, subject string, body []byte, builder ...Builder) {
 	go func() {
 		// TODO return error
-		_ = Sendmail(c, recipients, bcc, replyTo, subject, body)
+		_ = Sendmail(c, from, to, subject, body, builder...)
 	}()
 }
 
