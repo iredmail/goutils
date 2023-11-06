4ad863aea17cd11619e68431b764fb2c2d50a607
Zhang Huangbin <zhb@iredmail.org>
2023-11-06 23:28:52 +0800

    Typo

--- ctxutils/misc.go
+++ ctxutils/misc.go
@@ -14,23 +14,6 @@ func ReplyEmptyOK(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).SendString("")
 }
 
-func ApiRespError(ctx *fiber.Ctx, err error) error {
-	return ctx.JSON(fiber.Map{
-		"_success": false,
-		"_msg":     err.Error(),
-	})
-}
-
-func ApiRespData(ctx *fiber.Ctx, key string, data map[string]any) error {
-	m := fiber.Map{
-		"_success": true,
-		"_msg":     "",
-		key:        data,
-	}
-
-	return ctx.JSON(m)
-}
-
 func RouteURI(ctx *fiber.Ctx, name string, params fiber.Map) (uri string) {
 	uri, err := ctx.GetRouteURL(name, params)
 	if err != nil {
