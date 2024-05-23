afac87f8e4cd1f9606c03cca736333e59a2c752b
Zhang Huangbin <zhb@iredmail.org>
2024-05-23 15:26:20 +0800

    Add dbutils.DialectXX.

--- dbutils/db_type.go
+++ dbutils/db_type.go
@@ -5,10 +5,24 @@ const (
 	DBTypeMySQL    DBType = "mysql"
 	DBTypePGSQL    DBType = "pgsql"
 	DBTypeOpenLDAP DBType = "openldap"
+
+	DialectMySQL = "mysql"
+	DialectPg    = "postgres"
 )
 
 type DBType string
 
 func (t DBType) String() string {
 	return string(t)
+}
+
+func DBTypeToDialect(t DBType) (dialect string) {
+	switch t {
+	case DBTypePGSQL:
+		dialect = DialectPg
+	default:
+		dialect = DialectMySQL
+	}
+
+	return
 }
