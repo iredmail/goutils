501fdc074d108baef1849bed783a69e104dc917a
Zhang Huangbin <zhb@iredmail.org>
2024-10-10 15:18:33 +0800

    Remove Suffix.

--- dbutils/ldap.go
+++ dbutils/ldap.go
@@ -2,7 +2,6 @@ package dbutils
 
 type LDAPConnConfig struct {
 	URI                string
-	Suffix             string // dc=xx
 	BaseDN             string // o=domains,dc=xx
 	DomainAdminsBaseDN string // o=domainAdmins,dc=xx,dc=xx
 	BindDN             string
