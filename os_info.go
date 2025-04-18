ac609f82e17482818eeb5e0d4a7b1da99c38b07d
ly020044 <ly020044@outlook.com>
2025-04-18 11:23:41 +0800

    disks.go 的内容合并到 os_info.go

--- os_info.go
+++ os_info.go
@@ -2,6 +2,8 @@ package goutils
 
 import (
 	"bytes"
+	"cmp"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -11,6 +13,8 @@ import (
 	"slices"
 	"strings"
 
+	"github.com/dustin/go-humanize"
+	"github.com/shirou/gopsutil/v4/disk"
 	"github.com/shirou/gopsutil/v4/host"
 	"github.com/shirou/gopsutil/v4/mem"
 )
@@ -107,6 +111,14 @@ func (oi OSInfo) HasPGSQLLastLogin() bool {
 	}
 
 	return true
+}
+
+type DiskInfo struct {
+	Mounted     string `json:"mounted"`
+	Total       string `json:"total"`
+	Used        string `json:"used"`
+	Free        string `json:"free"`
+	UsedPercent int    `json:"used_percent"`
 }
 
 func GetOSInfo() (oi OSInfo, err error) {
@@ -287,6 +299,43 @@ func GetOSInfo() (oi OSInfo, err error) {
 	oi.UptimeDays = uptime / (60 * 60 * 24)
 	oi.UptimeHours = (uptime - (oi.UptimeDays * 60 * 60 * 24)) / (60 * 60)
 	oi.UptimeMinutes = ((uptime - (oi.UptimeDays * 60 * 60 * 24)) - (oi.UptimeHours * 60 * 60)) / 60
+
+	return
+}
+
+func GetDiskInfo() (dis []DiskInfo, err error) {
+	var partitionStats []disk.PartitionStat
+	partitionStats, err = disk.PartitionsWithContext(context.Background(), false)
+	if err != nil {
+		return
+	}
+
+	for _, p := range partitionStats {
+		if strings.HasPrefix(p.Mountpoint, "/snap/") {
+			continue
+		}
+
+		usage, err := disk.Usage(p.Mountpoint)
+		if err != nil {
+			continue
+		}
+
+		if usage.Total == 0 {
+			continue
+		}
+
+		dis = append(dis, DiskInfo{
+			Mounted:     p.Mountpoint,
+			Total:       humanize.IBytes(usage.Total),
+			Used:        humanize.IBytes(usage.Used),
+			Free:        humanize.IBytes(usage.Free),
+			UsedPercent: int(usage.UsedPercent),
+		})
+	}
+
+	slices.SortFunc(dis, func(a, b DiskInfo) int {
+		return cmp.Compare(a.Mounted, b.Mounted)
+	})
 
 	return
 }
