51a37247ec13369e2bde2684ae8798c5a27bcb26
Zhang Huangbin <2048991+iredmail@users.noreply.github.com>
2025-05-15 17:41:58 +0800

    Typo

--- logger/logger.go
+++ logger/logger.go
@@ -13,6 +13,78 @@ import (
 	"github.com/DeRuina/timberjack"
 )
 
+type logger struct {
+	sl *slog.Logger
+	w  io.Writer
+
+	level          string // log level: info, warn, error, debug
+	maxSize        int
+	rotateInterval string // rotate interval. e.g. `12h` (12 hours), `1d` (1 day), `1w` (1 week), `1m` (1 month)
+	maxBackups     uint
+	timeFormat     string
+	compress       bool // compress rotated log file
+	filePerm       os.FileMode
+
+	// Buffer size defaults to (8 * 1024).
+	// Write to log file immediately if size is 0.
+	bufferSize int
+}
+
+func (l *logger) ParseLevel() (level slog.Level, priority syslog.Priority, err error) {
+	l.level = strings.ToLower(l.level)
+	switch l.level {
+	case "debug":
+		level = slog.LevelDebug
+		priority = syslog.LOG_DEBUG
+	case "error":
+		level = slog.LevelError
+		priority = syslog.LOG_ERR
+	case "warn":
+		level = slog.LevelWarn
+		priority = syslog.LOG_WARNING
+	case "info":
+		level = slog.LevelInfo
+		priority = syslog.LOG_INFO
+	default:
+		err = fmt.Errorf("invalid level: %s", l.level)
+	}
+
+	return
+}
+
+func (l *logger) formatTime(groups []string, a slog.Attr) slog.Attr {
+	if a.Key == slog.TimeKey && l.timeFormat != "" {
+		t := a.Value.Time()
+		a.Value = slog.StringValue(t.Format(l.timeFormat))
+	}
+
+	return a
+}
+
+//
+// 实现 Logger 接口。
+//
+
+func (l *logger) Info(msg string, args ...interface{}) {
+	l.sl.Info(fmt.Sprintf(msg, args...))
+}
+
+func (l *logger) Error(msg string, args ...interface{}) {
+	l.sl.Error(fmt.Sprintf(msg, args...))
+}
+
+func (l *logger) Warn(msg string, args ...interface{}) {
+	l.sl.Warn(fmt.Sprintf(msg, args...))
+}
+
+func (l *logger) Debug(msg string, args ...interface{}) {
+	l.sl.Debug(fmt.Sprintf(msg, args...))
+}
+
+// Write Only supported file target mode.
+func (l *logger) Write(p []byte) (int, error) {
+	return l.w.Write(p)
+}
 func NewStdoutLogger(opts ...Option) (LoggerWithWriter, error) {
 	l := &logger{
 		w:     os.Stdout,
@@ -129,77 +201,4 @@ func NewSyslogLogger(server, tag string, options ...Option) (LoggerWithWriter, e
 	l.sl = slog.New(h)
 
 	return l, nil
-}
-
-type logger struct {
-	sl *slog.Logger
-	w  io.Writer
-
-	level          string // log level: info, warn, error, debug
-	maxSize        int
-	rotateInterval string // rotate interval. e.g. `12h` (12 hours), `1d` (1 day), `1w` (1 week), `1m` (1 month)
-	maxBackups     uint
-	timeFormat     string
-	compress       bool // compress rotated log file
-	filePerm       os.FileMode
-
-	// Buffer size defaults to (8 * 1024).
-	// Write to log file immediately if size is 0.
-	bufferSize int
-}
-
-func (l *logger) ParseLevel() (level slog.Level, priority syslog.Priority, err error) {
-	l.level = strings.ToLower(l.level)
-	switch l.level {
-	case "debug":
-		level = slog.LevelDebug
-		priority = syslog.LOG_DEBUG
-	case "error":
-		level = slog.LevelError
-		priority = syslog.LOG_ERR
-	case "warn":
-		level = slog.LevelWarn
-		priority = syslog.LOG_WARNING
-	case "info":
-		level = slog.LevelInfo
-		priority = syslog.LOG_INFO
-	default:
-		err = fmt.Errorf("invalid level: %s", l.level)
-	}
-
-	return
-}
-
-func (l *logger) formatTime(groups []string, a slog.Attr) slog.Attr {
-	if a.Key == slog.TimeKey && l.timeFormat != "" {
-		t := a.Value.Time()
-		a.Value = slog.StringValue(t.Format(l.timeFormat))
-	}
-
-	return a
-}
-
-//
-// 实现 Logger 接口。
-//
-
-func (l *logger) Info(msg string, args ...interface{}) {
-	l.sl.Info(fmt.Sprintf(msg, args...))
-}
-
-func (l *logger) Error(msg string, args ...interface{}) {
-	l.sl.Error(fmt.Sprintf(msg, args...))
-}
-
-func (l *logger) Warn(msg string, args ...interface{}) {
-	l.sl.Warn(fmt.Sprintf(msg, args...))
-}
-
-func (l *logger) Debug(msg string, args ...interface{}) {
-	l.sl.Debug(fmt.Sprintf(msg, args...))
-}
-
-// Write Only supported file target mode.
-func (l *logger) Write(p []byte) (int, error) {
-	return l.w.Write(p)
 }
