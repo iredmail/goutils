54533351734ad3fc0828cc4000f232205e93ad18
ly020044 <ly020044@outlook.com>
2025-07-01 19:02:20 +0800

    Add function mime.DecodeHeader

---
+++ mime/mime.go
@@ -0,0 +1,25 @@
+package mime
+
+import (
+	"fmt"
+	"io"
+	"mime"
+	"strings"
+
+	"golang.org/x/text/encoding/charmap"
+)
+
+// DecodeHeader decodes a MIME encoded-word header according to RFC 2047,
+// with extended support for ISO-8859-9 (Latin-5) character set.
+func DecodeHeader(header string) (string, error) {
+	wd := &mime.WordDecoder{}
+	wd.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
+		if strings.EqualFold(charset, "iso-8859-9") {
+			return charmap.ISO8859_9.NewDecoder().Reader(input), nil
+		}
+
+		return nil, fmt.Errorf("unsupported charset: %s", charset)
+	}
+
+	return wd.DecodeHeader(header)
+}
