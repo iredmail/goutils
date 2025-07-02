6dfc2605f4bc19a5d9f60405c23e2dfa6f6cf496
Zhang Huangbin <2048991+iredmail@users.noreply.github.com>
2025-07-02 23:38:41 +0800

    Move mime/*.go to emailutils/.

--- emailutils/mime.go
+++ emailutils/mime.go
@@ -1,4 +1,4 @@
-package mime
+package emailutils
 
 import (
 	"fmt"
@@ -10,9 +10,11 @@ import (
 )
 
 // DecodeHeader decodes a MIME encoded-word header according to RFC 2047,
-// with extended support for ISO-8859-9 (Latin-5) character set.
-func DecodeHeader(header string) (string, error) {
+// with extended support for ISO-8859-9 (Latin-5, Turkish) character set.
+func DecodeHeader(v string) (string, error) {
 	wd := &mime.WordDecoder{}
+
+	// Handle iso-8859-9
 	wd.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
 		if strings.EqualFold(charset, "iso-8859-9") {
 			return charmap.ISO8859_9.NewDecoder().Reader(input), nil
@@ -21,5 +23,5 @@ func DecodeHeader(header string) (string, error) {
 		return nil, fmt.Errorf("unsupported charset: %s", charset)
 	}
 
-	return wd.DecodeHeader(header)
+	return wd.DecodeHeader(v)
 }
