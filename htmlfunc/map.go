d4d68249ee83b93e59b1a214f39aaa4b013cf9a3
Zhang Huangbin <2048991+iredmail@users.noreply.github.com>
2026-01-04 19:50:20 +0800

    New: htmlfunc.GetMapStringInt64().

--- htmlfunc/map.go
+++ htmlfunc/map.go
@@ -1,5 +1,7 @@
 package htmlfunc
 
+import "sort"
+
 // GetMapStringInt64 从 key 为 string 类型，值为 int64 的 map 里取值。
 func GetMapStringInt64(m map[string]int64, key string) int64 {
 	if v, ok := m[key]; ok {
@@ -14,6 +16,27 @@ func GetMapInt64String(m map[int64]string, key int64) (s string) {
 	if v, ok := m[key]; ok {
 		return v
 	}
+
+	return
+}
+
+type KVStrInt struct {
+	K string
+	V int
+}
+
+func SortMapStrIntByIntDesc(m map[string]int) (kvs []KVStrInt) {
+	// FIXME 如何将值的类型由 int 改为 int64？
+	for k, v := range m {
+		kvs = append(kvs, KVStrInt{
+			K: k,
+			V: v,
+		})
+	}
+
+	sort.Slice(kvs, func(i, j int) bool {
+		return kvs[i].V > kvs[j].V
+	})
 
 	return
 }
