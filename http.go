a72a61c3de3efc71126357cc45aca9f9257aa569
Zhang Huangbin <2048991+iredmail@users.noreply.github.com>
2026-01-09 17:09:42 +0800

    Rename DownloadFile() to DownloadFileWithGauger(), restore DownloadFile().

--- http.go
+++ http.go
@@ -12,37 +12,67 @@ type Gauger interface {
 	Progress(current, total uint64)
 }
 
-type progressReader struct {
+type gaugeReader struct {
 	io.Reader
+
 	total      uint64
 	current    uint64
 	lastUpdate int64
 	gaugers    []Gauger
 }
 
-func (pr *progressReader) Read(p []byte) (n int, err error) {
-	n, err = pr.Reader.Read(p)
-	pr.current += uint64(n)
-	if pr.current >= pr.total {
-		for _, gauger := range pr.gaugers {
-			gauger.Progress(pr.total, pr.total)
+func (gr *gaugeReader) Read(p []byte) (n int, err error) {
+	n, err = gr.Reader.Read(p)
+	gr.current += uint64(n)
+	if gr.current >= gr.total {
+		for _, gauger := range gr.gaugers {
+			gauger.Progress(gr.total, gr.total)
 		}
 
 		return
 	}
 
 	now := time.Now().UnixNano() / int64(time.Millisecond)
-	if now-pr.lastUpdate > 100 {
-		for _, gauger := range pr.gaugers {
-			gauger.Progress(pr.current, pr.total)
+	if now-gr.lastUpdate > 100 {
+		for _, gauger := range gr.gaugers {
+			gauger.Progress(gr.current, gr.total)
 		}
-		pr.lastUpdate = now
+		gr.lastUpdate = now
 	}
 
 	return
 }
 
-func DownloadFile(url, dest string, validateCert bool, gaugers ...Gauger) (err error) {
+func DownloadFile(url, dest string, validateCert bool) (err error) {
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: !validateCert,
+			},
+		},
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+
+	return err
+}
+
+func DownloadFileWithGauger(url, dest string, validateCert bool, gaugers ...Gauger) (err error) {
 	client := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
@@ -68,7 +98,7 @@ func DownloadFile(url, dest string, validateCert bool, gaugers ...Gauger) (err e
 
 	var reader io.Reader = resp.Body
 	if totalSize > 0 && len(gaugers) > 0 {
-		reader = &progressReader{
+		reader = &gaugeReader{
 			Reader:  reader,
 			total:   uint64(totalSize),
 			gaugers: gaugers,
