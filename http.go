197835d1d03b68684c213632c82ba4e80dbe3ab0
ly020044 <ly020044@outlook.com>
2026-01-09 11:46:43 +0800

    Add interface Processes.

--- http.go
+++ http.go
@@ -8,15 +8,46 @@ import (
 	"time"
 )
 
-func DownloadFile(url, dest string, validateCert bool) (err error) {
-	tlsConfig := &tls.Config{
-		InsecureSkipVerify: !validateCert,
+type Progresses interface {
+	Progress(current, total uint64)
+}
+
+type processReader struct {
+	io.Reader
+	total      uint64
+	current    uint64
+	lastUpdate int64
+	progresses []Progresses
+}
+
+func (pr *processReader) Read(p []byte) (n int, err error) {
+	n, err = pr.Reader.Read(p)
+	pr.current += uint64(n)
+	if pr.current >= pr.total {
+		for _, progress := range pr.progresses {
+			progress.Progress(pr.total, pr.total)
+		}
+
+		return
 	}
 
+	now := time.Now().UnixNano() / int64(time.Millisecond)
+	if now-pr.lastUpdate > 100 {
+		for _, progress := range pr.progresses {
+			progress.Progress(pr.current, pr.total)
+		}
+		pr.lastUpdate = now
+	}
+
+	return
+}
+
+func DownloadFile(url, dest string, validateCert bool, progresses ...Progresses) (err error) {
 	client := &http.Client{
-		Timeout: 30 * time.Second,
 		Transport: &http.Transport{
-			TLSClientConfig: tlsConfig,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: !validateCert,
+			},
 		},
 	}
 
@@ -24,17 +55,28 @@ func DownloadFile(url, dest string, validateCert bool) (err error) {
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+
+	totalSize := resp.ContentLength
 
 	// Create the file
 	out, err := os.Create(dest)
 	if err != nil {
 		return err
 	}
-	defer out.Close()
+
+	var reader io.Reader = resp.Body
+	if totalSize > 0 && len(progresses) > 0 {
+		reader = &processReader{
+			Reader:     reader,
+			total:      uint64(totalSize),
+			progresses: progresses,
+		}
+	}
 
 	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
+	_, err = io.Copy(out, reader)
+	_ = resp.Body.Close()
+	_ = out.Close()
 
 	return err
 }
